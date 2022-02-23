52353c203c623cfca44120cfc8c556289c97cc79
Bricen Bennett <bricen@email.sc.edu>
2022-02-23 10:48:19 -0500

    server

--- server/server/index.js
+++ server/server/index.js
@@ -15,7 +15,7 @@ app.listen(PORT, () =>console.log('server started on port: ' + PORT));
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://wonderful-feynman-2756d5.netlify.app",
     credentials: true,
 }));
 app.use(cookieParser());
