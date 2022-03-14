455e92018516804d95bd4b286f83b00983b9fe69
Bricen Bennett <59937683+bricenb@users.noreply.github.com>
2022-03-14 11:51:19 -0400

    Add files via upload

---
+++ server/routers/vaxloopup.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const puppeteer = require("puppeteer");
+
+router.get("/", async (req, res) => {
+    try {
+        const apple = "052E21A";
+        //console.log(apple);
+        JSON.stringify(apple)
+        //console.log(apple);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto("https://www.modernatx.com/covid19vaccine-eua/providers/vial-lookup");
+        //console.log(apple);
+        await page.type("#lotNumber", apple);
+        //console.log(apple);
+        await page.click(".sticky-minimize");
+        await page.click("#submitLotNumber");
+        await page.waitForNetworkIdle();
+        //await page.screenshot({path: "amazing.png", fullPage: true});
+
+        const info = await page.$eval(".lot-number-expiry", el => el.textContent);
+        console.log(info);
+        
+        await browser.close();
+
+        res.json(info);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+module.exports = router;
